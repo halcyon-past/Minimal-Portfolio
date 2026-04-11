5e15661cfc3ce3ed212bd7d44156bc138d0beb07
halcyon-past <titanssuperior@gmail.com>
2026-04-11 15:52:51 +0530

    corrected tailwind

--- src/pages/ProjectDetailsPage.jsx
+++ src/pages/ProjectDetailsPage.jsx
@@ -115,7 +115,7 @@ const ProjectDetailsPage = () => {
                 alt={project.alt} 
                 className="w-full h-full object-cover object-center transform transition-transform duration-700 group-hover:scale-105"
               />
-              <div className="absolute inset-0 bg-gradient-to-t from-black/30 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 rounded-3xl" />
+              <div className="absolute inset-0 bg-linear-to-t from-black/30 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 rounded-3xl" />
             </div>
           </motion.div>
         </div>
